2bb56320f640d03d646ecf6962bf87fe6fd78177
not-so-fat <yusuke.muraoka.0208@gmail.com>
2026-09-10 07:50:48 -0700

    Add WorkflowInstance and WorkflowEvent shared types (NOT-57)

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -13,6 +13,7 @@ export * from "./result-qa.js";
 export * from "./playbook-reflect.js";
 export * from "./issues.js";
 export * from "./worker-sessions.js";
+export * from "./workflow.js";
 
 export const RunStatus = z.enum([
   "queued",
